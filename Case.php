f012db6a1620fa18606ded919520ab399ffb6378
ammardanish2d <ammardanish2d@gmail.com>
2025-05-26 15:47:34 +0800

    Naruto Hensem

---
+++ Case.php
@@ -0,0 +1,38 @@
+<?php
+// Basic PHP variables
+$name = "Visitor";
+$age = 20;
+
+// Simple conditional
+if ($age >= 18) {
+    $status = "Adult";
+} else {
+    $status = "Minor";
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My First PHP Page</title>
+</head>
+<body>
+    <h1>Welcome to PHP!</h1>
+
+    <p>Hello, <?php echo $name; ?>. You are an <?php echo $status; ?>.</p>
+
+    <!-- Simple form using GET -->
+    <form method="get">
+        <label>Enter your name:</label>
+        <input type="text" name="username">
+        <button type="submit">Submit</button>
+    </form>
+
+    <?php
+    // Handle GET request from form
+    if (isset($_GET['username']) && $_GET['username'] != "") {
+        echo "<p>Nice to meet you, " . htmlspecialchars($_GET['username']) . "!</p>";
+    }
+    ?>
+</body>
+</html>
